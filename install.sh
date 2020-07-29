8f00338927dcdf77601d6089f3ef572397199830
Diego Alfonzo <62270362+ClownSaw@users.noreply.github.com>
2020-07-29 03:56:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,18 +5,17 @@ chmod +x iplocalizacion
 
 mv iplocalizacion /usr/bin/
 
-mv core /usr/bin/
+mv core /usr/bin
 
-mv logs /usr/bin/
+mv logs /usr/bin
 
 echo "[Desktop Entry]
 Name=iplocalizacion
 Encoding=UTF-8
-Exec=/usr/share/kali-menu/exec-in-shell "diego"
+Exec=/usr/share/kali-menu/exec-in-shell "iplocalizacion"
 Icon=/usr/bin/core/iplocalizacion
 StartupNotify=false
 Terminal=true
 Type=Application
 Categories=01-info-gathering;
 X-Kali-Package=iplocalizacion " >> /usr/share/applications/kali-iplocalizacion.desktop 
-
