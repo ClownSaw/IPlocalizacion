75a30736210a60991f5b5d19adc2109185992dee
Diego Alfonzo <62270362+ClownSaw@users.noreply.github.com>
2020-07-29 02:36:17 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!usr/bin/bash
+sudo pip3 install -r requirements.txt
+
+chmod +x iplocalizacion
+
+sudo mv iplocalizacion /usr/bin/
+
+sudo mv core /usr/bin/
+
+sudo mv logs /usr/bin/
+
+sudo mv kali-iplocalizacion.desktop /usr/share/applications/
+
