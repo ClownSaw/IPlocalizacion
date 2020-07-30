0c6d1097977b1fa30de1108839a6c6710777a849
Diego Alfonzo <62270362+ClownSaw@users.noreply.github.com>
2020-07-30 06:57:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
 #!usr/bin/bash
+if which iplocalizacion >/dev/null; then
+                sleep 0.25
+    echo "[IPLLOCALIZACION]...........Installed [✓]"
+else
+                sleep 0.25
+        echo "[IPLOCALIZACION]..........Not Installed [✗]"
+                        sleep 1
+                echo "Install IPLOCALIZACION "
+                   echo "[Desktop Entry]
+Name=iplocalizacion
+Encoding=UTF-8
+Exec=/usr/share/kali-menu/exec-in-shell "iplocalizacion"
+Icon=/usr/bin/core/iplocalizacion
+StartupNotify=false
+Terminal=true
+Type=Application
+Categories=01-info-gathering;
+X-Kali-Package=iplocalizacion " >> /usr/share/applications/kali-iplocalizacion.desktop 
+
+fi
 pip3 install -r requirements.txt
 
 chmod +x iplocalizacion
@@ -9,13 +29,3 @@ mv core /usr/bin
 
 mv logs /usr/bin
 
-echo "[Desktop Entry]
-Name=iplocalizacion
-Encoding=UTF-8
-Exec=/usr/share/kali-menu/exec-in-shell "iplocalizacion"
-Icon=/usr/bin/core/iplocalizacion
-StartupNotify=false
-Terminal=true
-Type=Application
-Categories=01-info-gathering;
-X-Kali-Package=iplocalizacion " >> /usr/share/applications/kali-iplocalizacion.desktop 
